7fa6c6a55f65cf13e5d51883b9d7a4272f2a5729
root <root@Node62>
2017-07-07 13:26:41 +0530

    Implemented add customer functionality

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+// including routes files
 var index = require('./routes/index');
 var users = require('./routes/users');
 var customers = require('./routes/customers');
@@ -22,10 +23,11 @@ app.set('view engine', 'ejs');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Mysql connection
 app.use(
     connection(mysql,{ 
         host: 'localhost',
@@ -38,6 +40,7 @@ app.use(
 
 );
 
+// Handling user requests
 app.use('/', index);
 app.use('/users', users);
 app.use('/customers', customers);
--- routes/customers.js
+++ routes/customers.js
@@ -2,36 +2,66 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-    req.getConnection(function(err,connection){
-       
-        var query = connection.query('SELECT * FROM customer',function(err,rows)
+router.get('/', function (req, res, next) {
+    req.getConnection(function (err, connection) {
+
+        var query = connection.query('SELECT * FROM customer ORDER BY `id` DESC ', function (err, rows)
         {
-            
-            if(err)
-                console.log("Error Selecting : %s ",err );
-     
-            res.render('customers/index',{title:"Customers - Node.js",data:rows});
-                
-           
-         });
-         
-         //console.log(query.sql);
+
+            if (err)
+                console.log("Error Selecting : %s ", err);
+
+            res.render('customers/index', {title: "Customers - Node.js", data: rows});
+
+
+        });
+
+        //console.log(query.sql);
     });
 
 });
 
-router.get('/add', function(req, res, next) {
-  res.render('customers/add', { title: 'Add Customer' });
+/**
+ * function to load add customer page
+ */
+router.get('/add', function (req, res, next) {
+    res.render('customers/add', {title: 'Add Customer'});
 });
 
-router.get('/edit/:customerId(\d+)', function(req, res, next) {
-  console.log(req.params);
-  res.render('customers/edit', { title: 'Edit Customer' });
+/**
+ * function to save customer data
+ */
+router.post('/save', function (req, res) {
+    var customerData = req.body;
+    req.getConnection(function (err, connection) {
+        var data = {
+            name: customerData.name,
+            address: customerData.address,
+            email: customerData.email,
+            phone: customerData.phone
+
+        };
+        var query = connection.query("INSERT INTO customer set ? ", data, function (err, rows)
+        {
+            if (err)
+                console.log("Error inserting : %s ", err);
+
+            res.redirect('/customers');
+
+        });
+
+        // console.log(query.sql); get raw query
+
+    });
 });
 
-router.get('/delete/:customerId(\d+)', function(req, res, next) {
-  res.render('customers/delete', { title: 'Delete Customer' });
+router.get('/edit/:customerId(\d+)', function (req, res, next) {
+    console.log(req.params);
+    res.render('customers/edit', {title: 'Edit Customer'});
+});
+
+router.get('/delete/:customerId(\d+)', function (req, res, next) {
+    res.render('customers/delete', {title: 'Delete Customer'});
 });
 
 module.exports = router;
