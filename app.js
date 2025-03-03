9634a122dd5ae4626c7dd1a8f1cb24de9a71d2a4
Naveen <naveen@neuronimbus.com>
2025-03-03 13:08:25 +0530

    ui changes login page

--- app.js
+++ app.js
@@ -1,82 +1,86 @@
-const express = require('express');
-const path = require('path');
-const favicon = require('serve-favicon');
-const logger = require('morgan');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-var Tokens = require('csrf')
+const express = require("express");
+const path = require("path");
+const favicon = require("serve-favicon");
+const logger = require("morgan");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+var Tokens = require("csrf");
 
-let tokens = new Tokens()
- const token = tokens.secret().then(function (secret) {
+let tokens = new Tokens();
+const token = tokens.secret().then(function (secret) {
   // do something with the secret
   console.log(secret);
 });
- 
 
 // including routes files
-let index = require('./routes/index');
-let users = require('./routes/users');
-let customers = require('./routes/customers');
+let index = require("./routes/index");
+let users = require("./routes/users");
+let customers = require("./routes/customers");
 
 let app = express();
 
-let connection  = require('express-myconnection'); 
-let mysql = require('mysql');
+let connection = require("express-myconnection");
+let mysql = require("mysql");
 
-let session = require('express-session');
+let session = require("express-session");
+const AuthMiddleware = require("./middlewares/Auth");
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Mysql connection
 app.use(
-    connection(mysql,{ 
-        host: 'localhost',
-        user: 'root',
-        password : '',
-        port : 3306, //port mysql
-        database:'nodejs'
-
-    },'pool') //or single
-
+  connection(
+    mysql,
+    {
+      host: "localhost",
+      user: "root",
+      password: "",
+      port: 3306, //port mysql
+      database: "nodejs",
+    },
+    "pool"
+  ) //or single
 );
 
-app.use(session({
-    secret: 'secret',
+app.use(
+  session({
+    secret: "secret",
     resave: true,
-    saveUninitialized: true
-}));
+    saveUninitialized: true,
+  })
+);
 
 // Handling user requests
-app.use('/', index);
-app.use('/users', users);
-app.use('/customers', customers);
+app.use("/", index);
+app.use("/users", AuthMiddleware, users);
+app.use("/customers", AuthMiddleware, customers);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+app.use(function (req, res, next) {
+  var err = new Error("Not Found");
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -1,46 +1,48 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 /**
  * function to handle login
  */
-router.post('/auth', (req, res, next) => {
-    let loginData = req.body;
-    let username = loginData.username;
-    let password = loginData.password;
+router.post("/auth", (req, res, next) => {
+  let loginData = req.body;
+  let username = loginData.username;
+  let password = loginData.password;
 
-    if (username && password) {
-        req.getConnection(function (err, connection) {
-            let data = [username, password];
-            console.log(data);
-            var query = connection.query("SELECT * FROM users WHERE username = ? AND password = ?", data, function (err, rows) {
-                if (err)
-                    console.log("Error inserting : %s ", err);
-                if (rows.length > 0) {
-                    // Authenticate the user
-                    req.session.loggedin = true;
-                    req.session.username = username;
-                    // Redirect to home page
-                    res.redirect('/customers');
-                } else {
-                    res.send('Incorrect Username and/or Password!');
-                }
-                res.end();
-            });
-//             console.log(query.sql); //get raw query
-        });
-    } else {
-        res.send('Please enter Username and Password!');
-        res.end();
-    }
-
+  if (username && password) {
+    req.getConnection(function (err, connection) {
+      let data = [username, password];
+      console.log(data);
+      var query = connection.query(
+        "SELECT * FROM users WHERE username = ? AND password = ?",
+        data,
+        function (err, rows) {
+          if (err) console.log("Error inserting : %s ", err);
+          if (rows.length > 0) {
+            // Authenticate the user
+            req.session.loggedin = true;
+            req.session.username = username;
+            // Redirect to home page
+            res.redirect("/customers");
+          } else {
+            res.send("Incorrect Username and/or Password!");
+          }
+          res.end();
+        }
+      );
+      //             console.log(query.sql); //get raw query
+    });
+  } else {
+    res.send("Please enter Username and Password!");
+    res.end();
+  }
 });
 
-router.get('/logout', (req, res, next) => {
-    req.session.destroy(function (err) {
-        // cannot access session here
-        res.redirect('/users/login');
-    });
+router.get("/logout", (req, res, next) => {
+  req.session.destroy(function (err) {
+    // cannot access session here
+    res.redirect("/");
+  });
 });
 
 module.exports = router;
